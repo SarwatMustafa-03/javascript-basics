9397f123eeb70cd1fef0247627613fc74d75da87
Sarwat Mustafa <sarwatmustafa03@gmail.com>
2026-02-26 19:54:07 +0500

    loops

--- loops/loops.js
+++ loops/loops.js
@@ -2,3 +2,7 @@ for (let index = 1; index < 10; index++) {
     const element = array[index];
     console.log(array[index])
 }
+for (let index = 1; index < 10; index++) {
+    const element = array[index];
+    console.log(array[index])
+}
